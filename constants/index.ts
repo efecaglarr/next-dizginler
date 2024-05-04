eac8ce9eaa2c37e689e84a5a891977aacf8af941
Mehmet Efe Çağlar <efecagglar@gmail.com>
2024-05-04 11:04:12 +0300

    Years of productions changed.

--- constants/index.ts
+++ constants/index.ts
@@ -38,16 +38,16 @@ export const manufacturers = [
 ];
 
 export const yearsOfProduction = [
-  { title: "Year", value: "", name: "" },
-  { title: "2015", value: "2015", name: "2015" },
-  { title: "2016", value: "2016", name: "2016" },
-  { title: "2017", value: "2017", name: "2017" },
-  { title: "2018", value: "2018", name: "2018" },
-  { title: "2019", value: "2019", name: "2019" },
-  { title: "2020", value: "2020", name: "2020" },
-  { title: "2021", value: "2021", name: "2021" },
-  { title: "2022", value: "2022", name: "2022" },
-  { title: "2023", value: "2023", name: "2023" },
+  { title: "Year", value: "", name: "Year" },
+  { name: "2015", value: "2015", },
+  { name: "2016", value: "2016", },
+  { name: "2017", value: "2017", },
+  { name: "2018", value: "2018", },
+  { name: "2019", value: "2019", },
+  { name: "2020", value: "2020", },
+  { name: "2021", value: "2021", },
+  { name: "2022", value: "2022", },
+  { name: "2023", value: "2023", },
 ];
 
 
